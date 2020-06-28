2e2589e104bc5b56ec9f6eb0df4f75957da585a5
yuxino <nbsawz@gmail.com>
2020-06-28 15:37:26 +0800

    chore: remove promise

--- src/getFileType.ts
+++ src/getFileType.ts
@@ -4,7 +4,7 @@ import mime from 'mime';
  * Asynchronous recursive file copying with Node.js
  * @param {string} path target to be get mimetype
  */
-export default async function getFileType(path: string) {
+export default function getFileType(path: string) {
   // check start with dot
   if (path.startsWith('.')) return { ext: 'unknow', mime: 'unknow' };
 
